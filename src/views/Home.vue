68b90a8ae1af05c01858dec6668eee5f0908de7e
Raymond Idengeli <ray.idengeli@gmail.com>
2022-12-14 14:11:34 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
   <div class="home">
     <h1>This is home</h1>
     <p class="red white--text" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ratione incidunt iste natus corrupti voluptates similique inventore, possimus sit eius quos harum enim repellendus? Culpa a perspiciatis perferendis! Numquam, blanditiis?</p>
-    <p class="pink lighten-5 red--text text-darken-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate unde magnam eius praesentium fugiat esse at deleniti reprehenderit vitae neque adipisci, optio itaque repellendus quibusdam? Officia exercitationem rerum esse reiciendis consequuntur laudantium saepe dolores, labore dicta nam eveniet fugiat fuga, quia dolorem rem. Aut, dolores tempora ipsum natus at doloremque maiores incidunt eligendi autem voluptate asperiores numquam quisquam eaque magni laborum mollitia ea repellat in laudantium optio. Possimus inventore sequi voluptatibus pariatur dolor accusantium, voluptas ad quia maiores ipsam doloribus iusto architecto animi rem numquam explicabo eum atque error, optio doloremque perspiciatis aperiam officia tempore tempora! Corporis, obcaecati adipisci.</p>
+    <p class="pink lighten-5 red--text text-darken-2 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate unde magnam eius praesentium fugiat esse at deleniti reprehenderit vitae neque adipisci, optio itaque repellendus quibusdam? Officia exercitationem rerum esse reiciendis consequuntur laudantium saepe dolores, labore dicta nam eveniet fugiat fuga, quia dolorem rem. Aut, dolores tempora ipsum natus at doloremque maiores incidunt eligendi autem voluptate asperiores numquam quisquam eaque magni laborum mollitia ea repellat in laudantium optio. Possimus inventore sequi voluptatibus pariatur dolor accusantium, voluptas ad quia maiores ipsam doloribus iusto architecto animi rem numquam explicabo eum atque error, optio doloremque perspiciatis aperiam officia tempore tempora! Corporis, obcaecati adipisci.</p>
 
-     <h1 class="display-4">Massive header text</h1>
-     <h1 class="display-3">Massive header text</h1>
-     <h1 class="display-2">Massive header text</h1>
-     <h1 class="display-1 900">smaller display</h1>
-     <h1 class="body-2">text body class</h1>
+     <h1 class="display-4">Massive header text 4</h1>
+     <h1 class="display-3">Massive header text 3</h1>
+     <h1 class="display-2">Massive header text 2</h1>
+     <h1 class="display-1 font-weight-black">smaller display 1</h1>
+      <h1 class="body-1">text body class 1</h1>
+     <h1 class="body-2">text body class 2</h1>
      <p class="caption">Caption</p>
+     <p class="text-uppercase">text transform uppercase</p>
   </div>
 </template>
 
